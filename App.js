cebc1c25d0b279c1e26bfc1d4fd3b10fb640265f
Xiaotong Guan <xiaotongguan@xiaotongdembp.nunet.neu.edu>
2023-03-22 17:36:31 -0700

    create authentication components

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+// navigation
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+//style
+import { headerStyle } from "./helper/helper";
+// authentication screens
+import Login from "./authenticationcomponents/Login";
+import Signup from "./authenticationcomponents/Signup";
+
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={headerStyle}>
+        
+        <Stack.Screen name="Login" component={Login} />
+        <Stack.Screen name="Signup" component={Signup} />
+        
+        
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
---
+++ firebase/firebase-setup.js
@@ -0,0 +1,26 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+// https://firebase.google.com/docs/web/setup#available-libraries
+import {initializeAuth,getReactNativePersistence } from "firebase/auth"
+import AsyncStorage from '@react-native-async-storage/async-storage'
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD9lpNVN22HxNO0xnP33Z8kmKyyqAGuzOg",
+  authDomain: "trip-advisor-curd.firebaseapp.com",
+  projectId: "trip-advisor-curd",
+  storageBucket: "trip-advisor-curd.appspot.com",
+  messagingSenderId: "843970286871",
+  appId: "1:843970286871:web:6d6a6dc2cfa5e3087d1352"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+// Initialize Cloud Firestore through Firebase
+export const firestore = getFirestore(app);
+// export const auth = getAuth(myApp);
+export const auth = initializeAuth(myApp, {
+    persistence: getReactNativePersistence(AsyncStorage)
+  });
