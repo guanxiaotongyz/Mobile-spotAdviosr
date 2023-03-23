407e862df7431975a2b411fedad5e0f2e69ec7b3
Xiaotong Guan <xiaotongguan@XiaotongdeMacBook-Pro.local>
2023-03-22 22:21:56 -0700

    add style of sign up

--- authenticationcomponents/Signup.js
+++ authenticationcomponents/Signup.js
@@ -1,65 +1,88 @@
-import { View, Text, TextInput, Button, Alert } from "react-native";
+import { View, Text, TextInput, Button, Alert , StyleSheet } from "react-native";
 import React, { useState } from "react";
 import { auth } from "../firebase/firebase-setup";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 
 export default function Signup({ navigation }) {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const [confirmPassword, setConfirmPassword] = useState("");
-  
-    const loginHandler = () => {
-      navigation.replace("Login");
-    };
-  
-    const signupHandler = async () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
 
-      console.log("email", email)
-      if (password !== confirmPassword)
-      {
-          Alert.alert("The passwords don't match")
-          return;
-      }
-      try {
-        const userCred = await createUserWithEmailAndPassword(
-            auth,
-            email,
-            password
-          );
-      } catch (err) {
-        console.log("Auth error ", err);
-        Alert.alert("Error", err.message);
-      }
-    };
-    return (
-      <View>
-        <Text>Email</Text>
-        <TextInput
-          value={email}
-          onChangeText={(newEmail) => {
-            setEmail(newEmail);
-          }}
-          placeholder="Email" 
-       />
-        <Text>Password</Text>
-        <TextInput
-          value={password}
-          onChangeText={(newPassword) => {
-            setPassword(newPassword);
-          }}
-          placeholder="Password  "
-        />
+  const loginHandler = () => {
+    navigation.replace("Login");
+  };
+  const signupHandler = async () => {
+    if (password !== confirmPassword) {
+      //checking value and type {
+      Alert.alert("The passwords don't match");
+    }
+    try {
+      const userCred = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
+      console.log(userCred);
+    } catch (err) {
+      console.log("sign up error ", err);
+    }
+  };
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>New Email:</Text>
+      <TextInput
+        style={styles.input}
+        value={email}
+        onChangeText={(newEmail) => {
+          setEmail(newEmail);
+        }}
+        placeholder="New Email"
+      />
+      <Text style={styles.label}>New Password:</Text>
+      <TextInput
+        style={styles.input}
+        secureTextEntry={true}
+        placeholder="New Password"
+        value={password}
+        onChangeText={(newPassword) => {
+          setPassword(newPassword);
+        }}
+      />
+      <Text style={styles.label}>Confirm New Password:</Text>
+      <TextInput
+        style={styles.input}
+        secureTextEntry={true}
+        placeholder="Confirm New Password"
+        value={confirmPassword}
+        onChangeText={(newPassword) => {
+          setConfirmPassword(newPassword);
+        }}
+      />
+      <Button title="Register" onPress={signupHandler} />
+      <Button title="Already Registered? Login" onPress={loginHandler} />
+    </View>
+  );
+}
 
-        <Text>Confirm Password</Text>
-        <TextInput
-          value={confirmPassword}
-          onChangeText={(newPassword) => {
-            setConfirmPassword(newPassword);
-          }}
-          placeholder=" Confrim Password"
-        />
-        <Button title="Register" onPress={signupHandler} />
-        <Button title="Already Registered? Login" onPress={loginHandler} />
-      </View>
-    );
-  }
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "stretch",
+    justifyContent: "start",
+  },
+  input: {
+    borderColor: "#552055",
+    borderWidth: 2,
+    width: "90%",
+    marginTop: 10,
+    marginLeft: 10,
+    padding: 9,
+    borderRadius: 5,
+  },
+  label: {
+    marginTop: 10,
+    marginLeft: 10,
+    fontSize: 18,
+  },
+});
